8a751584263522f5ba7d897387a665a32a35a1b0
Matt Gill <matthew.gill@live.co.uk>
2019-10-30 15:46:56 +0000

    Add restart command

---
+++ src/commands/restart.js
@@ -0,0 +1,17 @@
+/**
+ * @typedef {import('yargs').Arguments} Arguments
+ */
+import { asadmin, builder as asadminBuilder } from './asadmin';
+
+export const command = 'restart';
+export const desc = 'Restart the running Payara instance';
+
+export const builder = argv => asadminBuilder(argv);
+
+/**
+ * @param {Arguments} argv the Yargs arguments
+ */
+export const handler = argv => {
+  console.log('Restarting Payara Server...');
+  asadmin('restart-domain');
+};
