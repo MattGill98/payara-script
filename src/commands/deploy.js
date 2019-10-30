8970bcdbf913e8aed06111144254031960257a51
Matt Gill <matthew.gill@live.co.uk>
2019-10-30 15:53:33 +0000

    Add deploy command

---
+++ src/commands/deploy.js
@@ -0,0 +1,24 @@
+/**
+ * Import Yargs type definitions
+ * 
+ * @typedef {import('yargs').Argv} Argv
+ * @typedef {import('yargs').Arguments} Arguments
+ */
+import { asadmin, builder as asadminBuilder } from './asadmin';
+
+// Command Details
+export const command = 'deploy <app>';
+export const desc = 'Deploy an application to the running Payara instance';
+
+/**
+ * @param {Argv} argv the Yargs instance
+ */
+export const builder = argv => asadminBuilder(argv);
+
+/**
+ * @param {Arguments} argv the Yargs arguments
+ */
+export const handler = argv => {
+  console.log('Deploying Payara Server...');
+  asadmin('deploy', '--force', '--name', 'app', '--contextroot', '/', argv.app);
+};
