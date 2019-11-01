82507c7365627e1f7c794c1f3351a6bf7a648ab6
Matt Gill <matthew.gill@live.co.uk>
2019-11-01 10:10:39 +0000

    Allow installing local ZIP

--- src/commands/install.js
+++ src/commands/install.js
@@ -14,7 +14,7 @@ import url from 'mvn-artifact-url';
 import download, { exists } from '../util/download';
 import { unzip } from './reset';
 
-export const command = 'install <name>';
+export const command = 'install [options] <artifact>';
 export const desc = 'Install a Payara environment from Maven';
 
 /**
@@ -24,10 +24,23 @@ export const builder = argv =>
   argv
     .help()
     .check(argv => {
+      // If the selected artifact is a file
+      if (fs.existsSync(argv.artifact)) {
+        if (!argv.name) {
+          argv.name = 'local'
+        }
+      } else {
+        // Assume the artifact is a version
+        argv.name = argv.artifact;
+      }
       if (listPackages().includes(argv.name)) {
-        throw 'That version is already installed.';
+        throw 'That artifact is already installed.';
       }
       return true;
+    })
+    .option('name', {
+      alias: 'n',
+      description: 'The name to install the artifact under'
     });
 
 const repositories = [
@@ -53,53 +66,62 @@ export const handler = argv => {
     // Create the install directory
     fs.mkdir(dir)
       .then(() => {
-        /**
-         * The artifact to download
-         * @type Artifact
-         */
-        let artifact = {
-          groupId: 'fish.payara.distributions',
-          artifactId: 'payara',
-          version: argv.name,
-          extension: 'zip'
-        };
+        if (fs.existsSync(argv.artifact)) {
+          // Copy the ZIP
+          fs.copyFile(argv.artifact, path.resolve(dir, globals.ZIP_NAME))
+          .then(() => {
+            // Unzip the artifact
+            unzip(dir);
+          })
+          .catch(handleError('Failed to rename ZIP file'));
+        } else {
+          /**
+           * The artifact to download
+           * @type Artifact
+           */
+          let artifact = {
+            groupId: 'fish.payara.distributions',
+            artifactId: 'payara',
+            version: argv.artifact,
+            extension: 'zip'
+          };
+
+          let urls = repositories.map(repository => url(artifact, repository));
   
-        let urls = repositories.map(repository => url(artifact, repository));
-  
-        // Resolve all URLs to download from
-        Promise.all(urls).then(resolvedUrls => {
-  
-          // Get the first promised URL that can be downloaded by inverting the promises
-          var invertedPromiseList = resolvedUrls
-            .map(resolvedUrl => exists(resolvedUrl, username, password))
-            .map(promise => promise.then(result => Promise.reject(result), err => Promise.resolve(err)));
-  
-          // Break on early exiting promise
-          Promise.all(invertedPromiseList)
-              .then(statusCodes => {
-                if (statusCodes.includes(401)) {
-                  console.error('Nexus authentication failed.');
-                } else {
-                  console.error('Artifact not found.');
-                }
-                rimraf(dir, () => {});
-              })
-              .catch(resolved => {
-                // Download the artifact
-                download(resolved, dir, username, password).then(artifact => {
-                  console.log('Downloaded Payara from maven.');
-                  // Rename the ZIP
-                  fs.rename(artifact, path.resolve(dir, globals.ZIP_NAME))
-                    .then(() => {
-                      // Unzip the artifact
-                      unzip(dir);
-                    })
-                    .catch(handleError('Failed to rename ZIP file'));
+          // Resolve all URLs to download from
+          Promise.all(urls).then(resolvedUrls => {
+    
+            // Get the first promised URL that can be downloaded by inverting the promises
+            var invertedPromiseList = resolvedUrls
+              .map(resolvedUrl => exists(resolvedUrl, username, password))
+              .map(promise => promise.then(result => Promise.reject(result), err => Promise.resolve(err)));
+    
+            // Break on early exiting promise
+            Promise.all(invertedPromiseList)
+                .then(statusCodes => {
+                  if (statusCodes.includes(401)) {
+                    console.error('Nexus authentication failed.');
+                  } else {
+                    console.error('Artifact not found.');
+                  }
+                  rimraf(dir, () => {});
                 })
-                .catch(handleError('Download failed', () => rimraf(dir, () => {})))
-              })
-        });
-      })
-      .catch(handleError('Failed to create directory for artifact'));
+                .catch(resolved => {
+                  // Download the artifact
+                  download(resolved, dir, username, password).then(artifact => {
+                    console.log('Downloaded Payara from maven.');
+                    // Rename the ZIP
+                    fs.rename(artifact, path.resolve(dir, globals.ZIP_NAME))
+                      .then(() => {
+                        // Unzip the artifact
+                        unzip(dir);
+                      })
+                      .catch(handleError('Failed to rename ZIP file'));
+                  })
+                  .catch(handleError('Download failed', () => rimraf(dir, () => {})));
+                });
+          });
+        }
+      }).catch(handleError('Failed to create directory for artifact'));
   });
 };
